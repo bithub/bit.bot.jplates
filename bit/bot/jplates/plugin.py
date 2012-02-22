15cd570bb2214ed29f00e8d69a4c40ea9224e98c
Ryan Northey <ryan@3ca.org.uk>
2012-02-22 23:00:23 +0000

    zcml

--- bit/bot/jplates/plugin.py
+++ bit/bot/jplates/plugin.py
@@ -5,13 +5,3 @@ from bit.core.interfaces import IPlugin
 from bit.bot.common.interfaces import IResourceRegistry
 from bit.bot.base.plugin import BotPlugin
 
-
-class BotJPlates(BotPlugin):
-    implements(IPlugin)
-    name = 'bit.bot.jplates'
-    _http = {'root': 'resources'}
-    def load_JS(self):
-        js = getUtility(IResourceRegistry,'js')
-        js.add('jquery.tmpl.min.js',{'rel':'link'})
-        js.add('jplates/jquery.jplates.js',{'rel':'link'})
-
